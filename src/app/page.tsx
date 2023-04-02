85b2eacf8607ea997df5c986409e6bada672b8b8
Jongbum <amjong2@gmail.com>
2023-04-02 16:36:08 +0900

    Add PostCard ui component, styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import { Inter } from 'next/font/google';
 import styles from './page.module.css';
 import { fetchData } from './util/fetchdata';
+import PostCard from './component/PostCard';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -9,13 +10,27 @@ export default function Home() {
   const data = fetchData();
   return (
     <>
-      {
-        <ul>
-          {data.map((post, index) => (
-            <li key={index}>{post.title}</li>
-          ))}
+      <section>
+        <h1 className='font-bold text-2xl ml-10'>Featured Post</h1>
+        <ul className='ml-10 mr-10 grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4'>
+          {data.map(
+            ({ title, description, date, category, path, featured }, index) => {
+              if (featured) {
+                return (
+                  <PostCard
+                    key={index}
+                    title={title}
+                    description={description}
+                    date={date}
+                    category={category}
+                    path={path}
+                  />
+                );
+              }
+            }
+          )}
         </ul>
-      }
+      </section>
     </>
   );
 }
