75055a5b0204da8373cb8d8bf6bbb80e87dd9195
Jongbum <amjong2@gmail.com>
2023-05-01 16:02:00 +0900

    src/app/posts : Styling post page

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -19,20 +19,22 @@ export default function PostsDetailPage(props: Props) {
   }) as post;
   const TargetMd = fetchTargetPost(path);
   return (
-    <article>
+    <article className='rounded-2xl overflow-hidden bg-gray-100 shadow-lg m-4'>
       <Image
+        className='w-full h-1/5 max-h-[500px]'
         src={`/images/posts/${path}.png`}
         alt={title}
         width={760}
         height={420}
       />
-      <section>
-        <div>
+      <section className='flex flex-col p-4'>
+        <div className='flex items-center self-end text-sky-600'>
           <AiTwotoneCalendar />
-          <p>{date.toString()}</p>
+          <p className='font-semibold ml-2'>{date.toString()}</p>
         </div>
-        <h1>{title}</h1>
-        <p>{description}</p>
+        <h1 className='text-4xl font-bold'>{title}</h1>
+        <p className='text-xl font-bold'>{description}</p>
+        <div className='w-44 border-2 border-sky-600 mt-4 mb-8'></div>
         <MarkdownView post={TargetMd} />
       </section>
     </article>
