7a9397644bea57a77757517b3672f1c26889f787
Jongbum <amjong2@gmail.com>
2023-05-05 16:23:09 +0900

    component/MailForm, MyForm : Modify styling

--- src/app/component/MailForm.tsx
+++ src/app/component/MailForm.tsx
@@ -3,7 +3,7 @@ import MyForm from './MyForm';
 
 export default function MailForm() {
   return (
-    <div className='bg-slate-500 flex flex-col items-center'>
+    <div className='bg-slate-500 h-96'>
       <MyForm></MyForm>
     </div>
   );
--- src/app/component/MyForm.tsx
+++ src/app/component/MyForm.tsx
@@ -4,10 +4,10 @@ import Button from './Button';
 import { mailInfo, sendMail } from '../util/mailer';
 
 export default function MyForm() {
-  const handleSubmit = useCallback(({ from, to, subject, text }: mailInfo) => {
-    const info = sendMail({ from, to, subject, text });
-    console.log(info);
-  }, []);
+  //   const handleSubmit = useCallback(({ from, to, subject, text }: mailInfo) => {
+  //     const info = sendMail({ from, to, subject, text });
+  //     console.log(info);
+  //   }, []);
   const [mail, setMail] = useState<mailInfo>({
     from: '',
     to: '',
@@ -22,32 +22,29 @@ export default function MyForm() {
     }));
   };
   return (
-    <div>
+    <div className='flex flex-col items-center h-96'>
       <h1 className='text-white'>Your Email</h1>
       <input
-        width='300'
-        height='50'
+        className='w-4/5'
         type='text'
         name='from'
         onChange={handleChange}
       ></input>
       <h1 className='text-white'>Subject</h1>
       <input
-        width='300'
-        height='50'
+        className='w-4/5'
         type='text'
         name='subject'
         onChange={handleChange}
       ></input>
       <h1 className='text-white'>Message</h1>
       <input
-        width='300'
-        height='500'
+        className='w-4/5 h-3/5'
         type='text'
         name='text'
         onChange={handleChange}
       ></input>
-      <Button handleSubmit={handleSubmit}>Submit</Button>
+      <Button>Submit</Button>
     </div>
   );
 }
